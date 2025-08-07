1f56fb4a4d2c7b5799608d30011f7829496091ec
Debabrata Halder <halderdebabrata01@gmail.com>
2025-08-07 17:05:24 +0530

    new video english

--- js/english.js
+++ js/english.js
@@ -50,5 +50,66 @@ const videoData = [
     id: "k6hzbVgJN9Y6n7Ds3se",
     title: "VERB Exercise",
     date: "17-07-2025"
+  },
+  {
+    id: "k65XbxZZj2D6UBDAMEy",
+    title: "ADJECTIVE",
+    date: "19-07-2025"
+  },
+  {
+    id: "kYYEXkr2g9ts8HDAMEw",
+    title: "ADJECTIVE Exercise",
+    date: "21-07-2025"
+  },
+  {
+    id: "k3uTvW4QZm3TXrDAMGy",
+    title: "ARTICLE",
+    date: "22-07-2025"
+  },
+  {
+    id: "k2oG4AVIqjkbQkDAMKA",
+    title: "Article-2",
+    date: "23-07-2025"
+  },
+  {
+    id: "k1u08L9b15dywiDAMKC",
+    title: "ADVERB Exercise",
+    date: "24-07-2025"
+  },
+  {
+    id: "k42Gg0I4iTCX4vDAMKE",
+    title: "CONJUNCTION",
+    date: "25-07-2025"
+  },
+  {
+    id: "k3ta0C0qfmS1NiDAMNy",
+    title: "CONJUNCTION Exercise",
+    date: "28-07-2025"
+  },
+  {
+    id: "k3lk4KrwIk34G0DAMNA",
+    title: "PREPOSITION",
+    date: "29-07-2025"
+  },
+  {
+    id: "k4CiscYDCmd5YODAMPC",
+    title: "Pronoun Exercise",
+    date: "30-07-2025"
+  },
+  {
+    id: "k4OLhOcVkgXM2UDAMRk",
+    title: "TENSE",
+    date: "01-08-2025"
+  },
+  {
+    id: "k2px5QaEJ2NJEUDAN0i",
+    title: "Q,R,E",
+    date: "05-08-2025"
+  },
+  {
+    id: "k7AOiGQzgVXJUlDAN0k",
+    title: "Q,R,E",
+    date: "06-08-2025"
   }
+
 ];
